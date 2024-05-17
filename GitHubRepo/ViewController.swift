51e83f14ea69f6435bf1b658ef3b14b060993414
Jesse Chen <stoola20@icloud.com>
2024-05-17 13:56:37 +0800

    init proj

---
+++ GitHubRepo/ViewController.swift
@@ -0,0 +1,20 @@
+////
+//  ViewController.swift
+//  GitHubRepo
+//
+//  Created by 1710 on 2024/5/17.
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
